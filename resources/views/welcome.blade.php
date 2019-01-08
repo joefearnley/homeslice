95553baaa34f980176cb7936496da4f26ae28840
Joe Fearnley <joe.fearnley@gmail.com>
2019-01-08 10:11:00 -0500

    Updating views to show correct links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -68,13 +68,24 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @else
                     @endauth
                 </div>
             @endif
@@ -83,6 +94,15 @@
                 <div class="title m-b-md">
                     Home Slice
                 </div>
+                @guest
+                    <div class="links">
+                        <a href="{{ route('login') }}">{{ __('Login') }}</a>
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">{{ __('Sign Up') }}</a>
+                        @endif
+                    </div>
+                @else
+                @endguest
             </div>
         </div>
     </body>
