e0223df3803f157d0352e9c60a6087bcf8219df2
Joe Fearnley <joe.fearnley@gmail.com>
2019-01-31 10:33:05 -0500

    Adding redirect if logged in

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <h2>Bong, Bong!</h2>
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
