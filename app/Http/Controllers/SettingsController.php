62e711ad264efbb31f9b764c569559f2d99448ed
Joe Fearnley <joe.fearnley@gmail.com>
2019-02-12 12:41:53 -0500

    Adding username/email unique validation

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 
 class SettingsController extends Controller
 {
@@ -20,14 +21,19 @@ class SettingsController extends Controller
 
     public function update(Request $request)
     {
+        $user = Auth::user();
+
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'username' => ['required', 'string', 'max:50'],
-            'email' => ['required', 'string', 'email', 'max:255'],
-            'password' => ['string', 'min:6', 'confirmed'],
+            'email' => ['required', 'string', 'email', 'max:255',
+                Rule::unique('users')->ignore($user->id)
+            ],
+            'password' => ['string', 'min:6', 'confirmed',
+                Rule::unique('users')->ignore($user->id)
+            ],
         ]);
 
-        $user = Auth::user();
         $user->name = $request->input('name');
         $user->username = $request->input('username');
         $user->email = $request->input('email');
