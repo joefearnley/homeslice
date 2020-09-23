00005c21371497ea98ca3de947b9a49982480f55
Joe Fearnley <joe.fearnley@gmail.com>
2020-09-22 22:31:34 -0400

    adding edit delete and tag buttons

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,33 +1,121 @@
 <x-app-layout>
-    <div class="py-6">
+    <div class="py-3">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="p-6 sm:px-10 bg-white border-b border-gray-200">
-                    <div class="mt-1 text-2xl">
-                        <a href="https://testtesttest.com">
-                            Learn Javascript in 20 days
-                        </a>
+                <div class="flex justify-evenly px-6 py-3 sm:px-10 bg-white border-b border-gray-200">
+                    <div class="w-full md:w-1/2">
+                        <div class="mt-1 text-2xl">
+                            <a href="https://testtesttest.com">
+                                Learn Javascript in 20 days
+                            </a>
+                        </div>
+                        <div class="mt-1 text-gray-500">
+                            <a href="https://testtesttest.com">https://testtesttest.com</a>
+                        </div>
                     </div>
-                    <div class="mt-2 text-gray-500">
-                        <a href="https://testtesttest.com">https://testtesttest.com</a>
+                    <div class="w-full md:w-1/2 text-right self-center">
+                        <button class="bg-teal-400 hover:bg-teal-500 text-white font-bold py-2 px-4 border border-teal-100 rounded">
+                            Edit
+                        </button>
+                        <button class="bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 border border-red-100 rounded ml-5">
+                            Delete
+                        </button>
                     </div>
+                </div>
+                <div class="mt-4 text-gray-500 flex flex-wrap px-6 sm:px-10">
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        Dev
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        JavaScript
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        JS
+                    </button>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="py-6">
+    <div class="py-3">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="p-6 sm:px-10 bg-white border-b border-gray-200">
-                    <div class="mt-2 text-2xl">
-                        <a href="https://yahoo.com">
-                            Yahoo
-                        </a>
+                <div class="flex justify-evenly px-6 py-3 sm:px-10 bg-white border-b border-gray-200">
+                    <div class="w-full md:w-1/2">
+                        <div class="mt-1 text-2xl">
+                            <a href="https://yahoo.com">
+                                Yahoo
+                            </a>
+                        </div>
+                        <div class="mt-1 text-gray-500">
+                            <a href="https://yahoo.com">https://yahoo.com</a>
+                        </div>
                     </div>
-                    <div class="mt-6 text-gray-500">
-                        <a href="https://yahoo.com">https://yahoo.com</a>
+                    <div class="w-full md:w-1/2 text-right self-center">
+                        <button class="bg-teal-400 hover:bg-teal-500 text-white font-bold py-2 px-4 border border-teal-100 rounded">
+                            Edit
+                        </button>
+                        <button class="bg-red-400 hover:bg-red-500 text-white font-bold py-2 px-4 border border-red-100 rounded ml-5">
+                            Delete
+                        </button>
                     </div>
+                </div>
+                <div class="mt-4 text-gray-500 flex flex-wrap px-6 sm:px-10">
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        Dev
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        JavaScript
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        JS
+                    </button>
+
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        Dev
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        JavaScript
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        JS
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        Dev
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        JavaScript
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        JS
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        Dev
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        JavaScript
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        JS
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        Dev
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        JavaScript
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        JS
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        Dev
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        JavaScript
+                    </button>
+                    <button class="rounded-full bg-gray-200 px-2 py-1 text-xs font-bold mr-3 mb-3">
+                        JS
+                    </button>
                 </div>
             </div>
         </div>
