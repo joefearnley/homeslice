932f19c17e5d52031d7b7d89767b55adce588056
Joe Fearnley <joe.fearnley@gmail.com>
2020-09-24 21:19:11 -0400

    adding first livewire component

--- routes/web.php
+++ routes/web.php
@@ -5,21 +5,7 @@ use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BookmarkController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', [WelcomeController::class, 'index']);
-
-// Route::get('/home', [HomeController::class, 'index']);
-
 
 Route::middleware(['auth:sanctum', 'verified'])
     ->get('/home', [HomeController::class, 'index'])
