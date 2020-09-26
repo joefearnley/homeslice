e8e81dadb0f737167cf9133c5a3b2a9bfe70fa7f
Joe Fearnley <joe.fearnley@gmail.com>
2020-09-25 21:53:47 -0400

    Adding initial models and migrations

--- homeslice/bookmarks/admin.py
+++ homeslice/bookmarks/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Bookmark, Tag
+
+admin.site.register(Bookmark)
+admin.site.register(Tag)
--- homeslice/bookmarks/models.py
+++ homeslice/bookmarks/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Bookmark(models.Model):
+    user = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
+    name = models.CharField(max_length=150)
+    url = models.CharField(max_length=100)
+    notes = models.CharField(max_length=256)
+    created_date = models.DateTimeField()
+    updated_date = models.DateTimeField()
+
+class Tag(models.Model):
+    name = models.CharField(max_length=150)
+    bookmark = models.ForeignKey(Bookmark, on_delete=models.CASCADE)
+    created_date = models.DateTimeField()
+    updated_date = models.DateTimeField()
