bd362b0373cdee71dea906f24391dca54fa6c16e
Joe Fearnley <joe.fearnley@gmail.com>
2020-09-25 22:42:46 -0400

    Setting up homepage

---
+++ homeslice/bookmarks/urls.py
@@ -0,0 +1,7 @@
+# urls.py
+# from django.urls import path
+# from some_app.views import View
+
+# urlpatterns = [
+#     path('bookmarks/', AboutView.as_view()),
+# ]
--- homeslice/homeslice/urls.py
+++ homeslice/homeslice/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from .views import HomeView
 
 urlpatterns = [
+    url(r'^$', HomeView.as_view()),
+    url(r'^admin/', admin.site.urls),
     url(r'^admin/', admin.site.urls),
 ]
