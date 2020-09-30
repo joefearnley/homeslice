79d13d703ec0f9886e6aa11dabf4af128f3aed80
Joe Fearnley <joe.fearnley@gmail.com>
2020-09-29 22:12:29 -0400

    changing main bookmarks page along with tests for it

--- homeslice/bookmarks/models.py
+++ homeslice/bookmarks/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 
 class Bookmark(models.Model):
     user = models.ForeignKey(
@@ -11,11 +13,11 @@ class Bookmark(models.Model):
     name = models.CharField(max_length=150)
     url = models.CharField(max_length=100)
     notes = models.CharField(max_length=256, null=True, blank=True)
-    created_date = models.DateTimeField()
-    updated_date = models.DateTimeField()
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(default=timezone.now)
 
 class Tag(models.Model):
     name = models.CharField(max_length=150)
     bookmark = models.ForeignKey(Bookmark, on_delete=models.CASCADE)
-    created_date = models.DateTimeField()
-    updated_date = models.DateTimeField()
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(default=timezone.now)
--- homeslice/bookmarks/tests.py
+++ homeslice/bookmarks/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
+from bookmarks.models import Bookmark
 
 
 class BookmarksTest(TestCase):
@@ -26,3 +27,74 @@ class BookmarksTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
         self.assertContains(response, 'No bookmarks yet!')
         self.assertContains(response, 'Add one!')
+
+    def test_bookmarks_list_shows_users_bookmarks(self):
+        bookmark1 = Bookmark.objects.create(
+            user=self.user,
+            name = 'Bookmark 1',
+            url = 'https://www.google.com',
+        )
+
+        bookmark2 = Bookmark.objects.create(
+            user=self.user,
+            name = 'Bookmark 2',
+            url = 'https://www.yahoo.com',
+        )
+
+        self.client.force_login(self.user)
+
+        response = self.client.get('/bookmarks/')
+
+        self.assertEquals(response.status_code, 200)
+
+        self.assertContains(response, 'Bookmark 1')
+        self.assertContains(response, 'https://www.google.com')
+        self.assertContains(response, 'Bookmark 2')
+        self.assertContains(response, 'https://www.yahoo.com')
+
+
+    def test_bookmarks_list_shows_users_bookmarks_only(self):
+        ''' set up bookmarks for multiple users - main users should not 
+            see those that belong to the second users'''
+
+        user1 = User.objects.create_user(
+            username='user1',
+            email='user1_123@gmail.com',
+            password='top_secret'
+        )
+
+        user2 = User.objects.create_user(
+            username='user2',
+            email='user2_123@gmail.com',
+            password='top_secret'
+        )
+
+        bookmark1 = Bookmark.objects.create(
+            user=user1,
+            name = 'Bookmark 1',
+            url = 'https://www.google.com',
+        )
+
+        bookmark2 = Bookmark.objects.create(
+            user=user1,
+            name = 'Bookmark 2',
+            url = 'https://www.yahoo.com',
+        )
+
+        bookmark3 = Bookmark.objects.create(
+            user=user2,
+            name = 'Bookmark 3',
+            url = 'https://www.bing.com',
+        )
+
+        self.client.force_login(self.user)
+
+        response = self.client.get('/bookmarks/')
+
+        self.assertEquals(response.status_code, 200)
+
+        self.assertContains(response, 'Bookmark 1')
+        self.assertContains(response, 'https://www.google.com')
+
+        self.assertNotContains(response, 'Bookmark 3')
+        self.assertNotContains(response, 'https://www.bing.com')
