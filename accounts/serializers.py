14660b5f1b674621572e32f22fa60a63f6e95468
Joe Fearnley <joe.fearnley@gmail.com>
2022-10-16 14:55:45 -0400

    setting up api for accounts

---
+++ accounts/serializers.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import Group
+from rest_framework import serializers
+from .models import Account
+
+class AccountSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['url', 'username', 'email', 'groups']
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import AccountSerializer
+from .models import Account
 
-# Create your views here.
+
+class AccountViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Account.objects.all().order_by('-date_joined')
+    serializer_class = AccountSerializer
+    permission_classes = [permissions.IsAuthenticated]
