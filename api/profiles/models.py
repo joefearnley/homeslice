4ed74a1b32e354c1ee0b0cc47e952d3269718895
Joe Fearnley <joe.fearnley@gmail.com>
2022-10-26 22:23:40 -0400

    Chaning active to is_active in Profile and Link models

--- api/profiles/models.py
+++ api/profiles/models.py
@@ -1,18 +1,23 @@
 from django.db import models
+from accounts.models import Account
 
 
 class Profile(models.Model):
+    account = models.ForeignKey(
+        Account,
+        on_delete=models.CASCADE,
+    )
     name = models.CharField(max_length=100)
     username  = models.CharField(max_length=100)
     bio = models.TextField(blank=True)
-    active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
 
 
 class Link(models.Model):
     profile = models.ForeignKey(
-        'Profile',
+        Profile,
         on_delete=models.CASCADE,
     )
-    url = models.CharField(max_length=30, blank = False)
-    title = models.CharField(max_length=30, blank = False)
-    active = models.BooleanField(default=True)
+    url = models.CharField(max_length=30, blank=False)
+    title = models.CharField(max_length=30, blank=False)
+    is_active = models.BooleanField(default=True)
