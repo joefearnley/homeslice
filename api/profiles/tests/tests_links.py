a3cb2f1abbf6313cbcbd8f2ba6c30d7ffad20b52
Joe Fearnley <joe.fearnley@gmail.com>
2022-10-30 07:06:46 -0400

    Adding tests for deleting links

--- api/profiles/tests/tests_links.py
+++ api/profiles/tests/tests_links.py
@@ -47,16 +47,6 @@ class AccessLinksTest(APITestCase, LinkTestMixin):
 
         self.assertEquals(response.status_code, status.HTTP_200_OK)
 
-    def test_can_view_links_data(self):
-        self.authenticate_account()
-        self.create_link()
-
-        response = self.client.get('/api/v1/links/')
-
-        self.assertEquals(response.status_code, status.HTTP_200_OK)
-        self.assertContains(response, self.link.url)
-        self.assertContains(response, self.link.title)
-
     def test_can_only_view_links_data_for_authenticated_account(self):
         self.authenticate_account()
         self.create_link()
@@ -89,6 +79,16 @@ class AccessLinksTest(APITestCase, LinkTestMixin):
 
         self.assertNotContains(response, other_link.url)
         self.assertNotContains(response, other_link.title)
+
+    def test_can_view_links_data(self):
+        self.authenticate_account()
+        self.create_link()
+
+        response = self.client.get('/api/v1/links/')
+
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, self.link.url)
+        self.assertContains(response, self.link.title)
 
 
 class CreateLinksTest(APITestCase, LinkTestMixin):
@@ -286,6 +286,39 @@ class UpdateLinksTest(APITestCase, LinkTestMixin):
         self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['title'][0], 'This field may not be blank.')
 
+    def test_can_only_update_links_for_authenticated_account(self):
+        self.authenticate_account()
+        self.create_link()
+
+        other_account = Account.objects.create_user(
+            username='janedoe',
+            first_name='Jane',
+            last_name='Doe',
+            email='jane.b.doe@gmail.com',
+            password='top_secret'
+        )
+
+        other_profile = Profile.objects.create(
+            account=other_account,
+            title='The Jane Doe Home Page',
+            bio='this is a little somthing about me (the jane doe)'
+        )
+
+        other_link = Link.objects.create(
+            profile=other_profile,
+            url='https://www.instagram.com/janedoe456',
+            title='Jane Doe Instagram'
+        )
+
+        post_data = {
+            'url': 'https://instagram.com/jdoe1234',
+            'title': 'Instagram Account'
+        }
+
+        response = self.client.put('/api/v1/links/%s/' % other_link.id, post_data)
+
+        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
+
     def test_can_update_link(self):
         self.authenticate_account()
         self.create_link()
@@ -302,3 +335,51 @@ class UpdateLinksTest(APITestCase, LinkTestMixin):
         updated_link = Link.objects.get(id=self.link.id)
         self.assertEqual(updated_link.url, post_data['url'])
         self.assertEqual(updated_link.title, post_data['title'])
+
+
+class DeleteLinkTest(APITestCase, LinkTestMixin):
+    def test_cannot_delete_link_when_not_authenticated(self):
+        self.create_link()
+
+        response = self.client.delete('/api/v1/links/%s/' % self.link.id)
+
+        self.assertEquals(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_can_only_delete_links_for_authenticated_account(self):
+        self.authenticate_account()
+        self.create_link()
+
+        other_account = Account.objects.create_user(
+            username='janedoe',
+            first_name='Jane',
+            last_name='Doe',
+            email='jane.b.doe@gmail.com',
+            password='top_secret'
+        )
+
+        other_profile = Profile.objects.create(
+            account=other_account,
+            title='The Jane Doe Home Page',
+            bio='this is a little somthing about me (the jane doe)'
+        )
+
+        other_link = Link.objects.create(
+            profile=other_profile,
+            url='https://www.instagram.com/janedoe456',
+            title='Jane Doe Instagram'
+        )
+
+        response = self.client.delete('/api/v1/links/%s/' % other_link.id)
+
+        self.assertEquals(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_can_delete_links(self):
+        self.authenticate_account()
+        self.create_link()
+
+        response = self.client.delete('/api/v1/links/%s/' % self.link.id)
+
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        deleted_link = Link.objects.filter(id=self.link.id)
+        self.assertEquals(0, len(deleted_link))
