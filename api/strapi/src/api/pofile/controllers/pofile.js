0574e9c18d2c9c4b324a2cf33fb262014d44259d
Joe Fearnley <joe.fearnley@gmail.com>
2023-06-05 21:28:03 -0400

    adding strapi

---
+++ api/strapi/src/api/pofile/controllers/pofile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pofile controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::pofile.pofile');
