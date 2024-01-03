bf65847ca3edb972588710a733686b3662120c2a
Joe Fearnley <joe.fearnley@gmail.com>
2024-01-02 21:35:08 -0500

    Setting up views

--- django/homeslice/urls.py
+++ django/homeslice/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 from .views import HomeView, DashboardView
+from accounts.views import SignupView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('', DashboardView.as_view(), name='dashboard'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('admin/', admin.site.urls),
+
+    path('accounts/', include('django_registration.backends.activation.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 
     # include api urls
 ]
