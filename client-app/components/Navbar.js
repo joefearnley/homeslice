6902dc3519396db953d2938247a37e7ec76862d5
Joe Fearnley <joe.fearnley@gmail.com>
2024-03-13 22:23:49 -0400

    more changes to dashboard

--- client-app/components/Navbar.js
+++ client-app/components/Navbar.js
@@ -2,19 +2,17 @@ const Navbar = () => {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">Homeslice</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
                     <li><a>Link</a></li>
                     <li>
                         <details>
-                            <summary>
-                            Parent
-                            </summary>
+                            <summary>Parent</summary>
                             <ul className="p-2 bg-base-100 rounded-t-none">
-                            <li><a>Link 1</a></li>
-                            <li><a>Link 2</a></li>
+                                <li><a>Link 1</a></li>
+                                <li><a>Link 2</a></li>
                             </ul>
                         </details>
                     </li>
--- client-app/pages/dashboard.js
+++ client-app/pages/dashboard.js
@@ -2,7 +2,7 @@ import Navbar from '../components/Navbar'
 
 const Dashboard = () => {
     return (
-        <div>
+        <div className="container m-auto">   
             <Navbar />
             <div>
                 <h1>This is the Dashboard</h1>
