9095ecd6f3d33c32e6ca28a062a6c1a37bf02dff
Joe Fearnley <joe.fearnley@gmail.com>
2024-03-17 10:14:51 -0400

    Adding authlayout file

---
+++ client-app/pages/AuthLayout.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar'
+ 
+export default function AuthLayout({ children }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <Navbar />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
