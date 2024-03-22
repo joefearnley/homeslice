3b48fc6b034788c04c2e041d628c3095d9775687
Joe Fearnley <joe.fearnley@gmail.com>
2024-03-21 22:07:43 -0400

    Change up navbar

--- client-app/components/Navbar.js
+++ client-app/components/Navbar.js
@@ -2,21 +2,26 @@ const Navbar = () => {
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
-                <a className="btn btn-ghost text-xl">Homeslice</a>
+                <a className="btn btn-ghost text-xl">daisyUI</a>
             </div>
             <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Link</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2 bg-base-100 rounded-t-none">
-                                <li><a>Link 1</a></li>
-                                <li><a>Link 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                </ul>
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img alt="User Profile Image" src="#" />
+                        </div>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li>
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
--- client-app/pages/dashboard.js
+++ client-app/pages/dashboard.js
@@ -2,13 +2,13 @@ import Navbar from '../components/Navbar'
 
 const Dashboard = () => {
     return (
-        <div className="container m-auto">   
+        <div className="container m-auto bg-base-100">   
             <Navbar />
-            <div>
+            <div className="p-5">
                 <h1>This is the Dashboard</h1>
             </div>
         </div>
     )
 }
 
-export default Dashboard;
+export default Dashboard
