b3893201247efd222d43c21d438957891eea6f72
Joe Fearnley <joe.fearnley@gmail.com>
2024-04-05 20:42:04 -0400

    round corners

--- client-app/pages/account/settings.js
+++ client-app/pages/account/settings.js
@@ -69,7 +69,7 @@ const AccountSettings = () => {
             <article className="prose">
                 <h1 className="pb-8">Account Settings</h1>
             </article>
-            <div className="border p-5 w-full md:w-1/2">
+            <div className="border rounded-md p-5 w-full md:w-1/2">
                 <div role="alert" className={`alert alert-success mt-5 mb-8 ${ showAlert ? 'block' : 'hidden' }`}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                     <span>Your information has been udpated!</span>
