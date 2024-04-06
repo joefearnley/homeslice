5fdf747323ecafd117494be3603d6e1620a3dfe1
Joe Fearnley <joe.fearnley@gmail.com>
2024-04-05 20:36:14 -0400

    Getting account update formm working

--- client-app/pages/account/settings.js
+++ client-app/pages/account/settings.js
@@ -10,12 +10,7 @@ const AccountSettings = () => {
     const authToken = getCookie('homeslice_auth_token');
 
     const handleChange = (event) => {
-
         const { target } = event;
-
-        console.log(target.value);
-        console.log(target.name);
-
         setUser((prevState) => ({
             ...prevState,
             [target.name]: target.value,
@@ -25,12 +20,22 @@ const AccountSettings = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
 
+        console.log(user);
+
+        const bodyData = JSON.stringify({
+            username: user.username,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            email: user.email,
+        });
+
         const options = {
             method: 'PATCH',
             headers: {
+                'Content-Type': 'application/json',
                 'Authorization': `Token ${authToken}`,
             },
-            data: JSON.stringify(user),
+            body: bodyData,
         };
 
         console.log(options);
@@ -69,7 +74,7 @@ const AccountSettings = () => {
             <article className="prose">
                 <h1 className="pb-8">Account Settings</h1>
             </article>
-            <div className="border p-5 w-1/2">
+            <div className="border p-5 w-full md:w-1/2">
                 <div role="alert" className={`alert alert-success mt-5 mb-8 ${ showAlert ? 'block' : 'hidden' }`}>
                     <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
                     <span>Your information has been udpated!</span>
@@ -81,25 +86,25 @@ const AccountSettings = () => {
                     <div className="mb-5">
                         <label htmlFor="username">Username</label>
                         <div className="mt-3">
-                            <input type="text" value={ user.username || '' } onChange={handleChange} placeholder="@username" id="username" name="username" className="input input-bordered w-full max-w-xs" />
+                            <input type="text" value={ user.username || '' } onChange={handleChange} placeholder="@username" id="username" name="username" className="input input-bordered w-full" />
                         </div>
                     </div>
                     <div className="mb-5">
                         <label htmlFor="firstname">First Name</label>
                         <div className="mt-3">
-                            <input type="text" value={ user.first_name || ''} onChange={handleChange} placeholder="first name" id="first_name" name="first_name" className="input input-bordered w-full max-w-xs" />
+                            <input type="text" value={ user.first_name || ''} onChange={handleChange} placeholder="first name" id="first_name" name="first_name" className="input input-bordered w-full" />
                         </div>
                     </div>
                     <div className="mb-5">
                         <label htmlFor="lastname">Last Name</label>
                         <div className="mt-3">
-                            <input type="text" value={ user.last_name || '' } onChange={handleChange} placeholder="last name" id="lastname" name="last_name" className="input input-bordered w-full max-w-xs" />
+                            <input type="text" value={ user.last_name || '' } onChange={handleChange} placeholder="last name" id="lastname" name="last_name" className="input input-bordered w-full" />
                         </div>
                     </div>
                     <div className="mb-5">
                         <label htmlFor="email">Email Address</label>
                         <div className="mt-3">
-                            <input type="email" placeholder="name@email.com" onChange={handleChange} value={ user.email || '' }  id="email" name="email" className="input input-bordered w-full max-w-xs" />
+                            <input type="email" placeholder="name@email.com" onChange={handleChange} value={ user.email || '' }  id="email" name="email" className="input input-bordered w-full" />
                         </div>
                     </div>
                     <button type="submit" className="btn">Save</button>
