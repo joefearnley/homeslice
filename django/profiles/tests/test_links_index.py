6a66dcbc7768d69fe8bf52bf5e981ed66e11f06f
Joe Fearnley <joe.fearnley@gmail.com>
2025-06-20 22:57:27 -0400

    Fixing test

--- django/profiles/tests/test_links_index.py
+++ django/profiles/tests/test_links_index.py
@@ -7,13 +7,12 @@ from .test_links_base import LinkTestMixin
 class LinksIndexTest(APITestCase, LinkTestMixin):
     def test_cannot_access_link_index_page_when_not_authenticated(self):
         login_redirect_url = reverse_lazy('account_login') + '?next=' + reverse_lazy('link-index')
-
         response = self.client.get(reverse_lazy('link-index'))
 
         self.assertRedirects(response, login_redirect_url, status_code=302, target_status_code=200)
 
     def test_can_access_link_index_page_when_authenticated(self):
-
+        login_redirect_url = reverse_lazy('account_login') + '?next=' + reverse_lazy('link-index')
         response = self.client.get(reverse_lazy('link-index'))
 
         self.assertRedirects(response, login_redirect_url, status_code=302, target_status_code=200)
